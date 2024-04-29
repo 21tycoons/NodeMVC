f10b2d03f5c2e9f0e759331192f3c0bd4c2050df
Liroy Leshed <liroyleshed@Liroys-MacBook-Air.local>
2024-04-29 09:58:17 -0700

    update

--- config/initializer.js
+++ config/initializer.js
@@ -8,6 +8,6 @@ app.engine('mustache', mustacheExpress())
 app.engine('pug', pug.__express)
 
 
-const VIEW_ENGINES = { PUG: 'pug', EJS: 'ejs', Mustache: 'mustache' }
+const VIEW_ENGINES = { EJS: 'ejs', Mustache: 'mustache', PUG: 'pug' }
 app.set('view engine', VIEW_ENGINES.EJS)
 app.set('views', path.join(__dirname, '../app/views'))
