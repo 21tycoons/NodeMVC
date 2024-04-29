c6023a01e1b9fe40dadf4b2d943b49e38e99292a
Liroy Leshed <liroyleshed@Liroys-MacBook-Air.local>
2024-04-29 03:26:54 -0700

    add mustache support

--- config/initializer.js
+++ config/initializer.js
@@ -2,5 +2,11 @@ const app  = require('./server')
 const path = require('path')
 
 
+const mustacheExpress = require('mustache-express')
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache')
+app.set('views', path.join(__dirname, '../app/views'))
+
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../app/views'))
