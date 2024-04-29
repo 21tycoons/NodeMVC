7b9181b60e99531cb38163f075cbc8554ff5cf4f
Liroy Leshed <liroyleshed@Liroys-MacBook-Air.local>
2024-04-29 09:01:26 -0700

    update

--- config/initializer.js
+++ config/initializer.js
@@ -5,7 +5,5 @@ const path = require('path')
 const mustacheExpress = require('mustache-express')
 app.engine('mustache', mustacheExpress())
 app.set('view engine', 'mustache')
-
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../app/views'))
