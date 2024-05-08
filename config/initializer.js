593bea8a6dff1b9b3126adda1eca6f616d8a257d
Liroy Leshed <liroyleshed@protonmail.com>
2024-05-08 01:01:35 -0700

    Update initializer.js

--- config/initializer.js
+++ config/initializer.js
@@ -1,12 +1,10 @@
 const app  = require('./server')
 const path = require('path')
 
-
 const mustacheExpress = require('mustache-express')
 const pug = require('pug')
 app.engine('mustache', mustacheExpress())
 app.engine('pug', pug.__express)
-
 
 const VIEW_ENGINES = { EJS: 'ejs', Mustache: 'mustache', PUG: 'pug' }
 app.set('view engine', VIEW_ENGINES.EJS)
