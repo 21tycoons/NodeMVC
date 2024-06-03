135df4dec41e2f1740a7e055f56a1a8ec86da16e
Liroy Leshed <liroyleshed@protonmail.com>
2024-06-02 17:43:14 -0700

    Update initializer.js

--- config/initializer.js
+++ config/initializer.js
@@ -8,5 +8,5 @@ app.engine( 'pug', pug.__express )
 
 // Here you can set your desired template engine, options are the three below but you can add extra.
 const VIEW_ENGINES = { EJS: 'ejs', Mustache: 'mustache', PUG: 'pug' }
-app.set('view engine', VIEW_ENGINES.EJS)
-app.set('views', path.join(__dirname, '../app/views'))
+app.set( 'view engine', VIEW_ENGINES.EJS )
+app.set( 'views', path.join(__dirname, '../app/views') )
