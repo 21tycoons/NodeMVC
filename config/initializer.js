c468a6bdfb73e6060c6e90520cdd80c77f9151b4
Liroy Leshed <liroyleshed@Liroys-MacBook-Air.local>
2024-06-02 17:41:12 -0700

    push update

--- config/initializer.js
+++ config/initializer.js
@@ -6,6 +6,7 @@ const pug = require('pug')
 app.engine('mustache', mustacheExpress())
 app.engine('pug', pug.__express)
 
+// Here you can set your desired template engine, options are the three below but you can add extra.
 const VIEW_ENGINES = { EJS: 'ejs', Mustache: 'mustache', PUG: 'pug' }
 app.set('view engine', VIEW_ENGINES.EJS)
 app.set('views', path.join(__dirname, '../app/views'))
