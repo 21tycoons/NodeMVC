c16d972db5361d219e3253ed0844cca8451cce9f
Liroy Leshed <liroyleshed@Liroys-MacBook-Air.local>
2024-06-02 18:40:35 -0700

    push update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  content: ['./app/views/**/*.{ejs,html,js,pug}'],
   plugins: [
     require('daisyui')
-  ]
+  ],
+  theme: {
+  extend: {
+    fontFamily: {
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    },
+  },
+  },
 }
