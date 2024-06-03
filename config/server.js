50a854569c39d42467599ca4ef8667b6be5b77d3
Liroy Leshed <liroyleshed@Liroys-MacBook-Air.local>
2024-06-02 17:56:49 -0700

    push update

--- config/server.js
+++ config/server.js
@@ -24,7 +24,7 @@ require('../app/controllers/application_controller')
 const PORT = process.env.PORT || 3100
 
 /* istanbul ignore next */
-if (!module.parent) {
-  app.listen(PORT)
-  console.log('Express started on port ' + PORT)
+if ( !module.parent ) {
+  app.listen( PORT )
+  console.log( 'Express started on port ' + PORT )
 }
